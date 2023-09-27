7258389096a6a1a0bf1f8742229e7373ce2d54cc
felix <felix.widmayer@rwth-aachen.de>
2023-09-27 20:33:09 +0200

    Chapter 23

--- cpp_templates_the_complete_guide/chapter23_metaprogramming.hpp
+++ cpp_templates_the_complete_guide/chapter23_metaprogramming.hpp
@@ -27,7 +27,52 @@ using RemoveAllExtents = typename RemoveAllExtentsT<T>::Type;
 // 2) T = int[5]                         // known bound specialization
 // 3) T = int                            // base case
 
+// Hybrid metaprogramming for unit types in the style of std::chrono..
 
+template <unsigned N, unsigned D = 1> struct Ratio {
+  static constexpr unsigned num = N;
+  static constexpr unsigned den = D;
+  using Type = Ratio<num, den>;
+};
 
+// Adds two ratios, i.e. den1/num1 + den2/num2.
+template <typename R1, typename R2> class RatioAddImpl {
+private:
+  static constexpr unsigned den = R1::den * R2::den;
+  static constexpr unsigned num = R1::num * R2::den + R2::num * R1::den;
+
+public:
+  using Type = Ratio<num, den>;
+};
+template <typename R1, typename R2> using RatioAdd = RatioAddImpl<R1, R2>;
+
+// Duration type for values of type T with unit type U.
+template <typename T, typename U = Ratio<1>> class Duration {
+public:
+  using ValueType = T;
+  using UnitType = typename U::Type;
+
+private:
+  ValueType val_;
+
+public:
+  constexpr Duration(ValueType val = 0) noexcept : val_{val} {}
+  constexpr ValueType value() const noexcept { return val_; }
+};
+
+// Adds two durations with arbitrary unit types.
+template <typename T1, typename U1, typename T2, typename U2>
+constexpr auto operator+(const Duration<T1, U1> &lhs,
+                         const Duration<T2, U2> &rhs) {
+  // Resulting unit type has numerator of 1 and denominator as a result of
+  // adding both fractions.
+  using RR = Ratio<1, RatioAdd<U1, U2>::den>;
+
+  // Sum of both values converted to the resulting unit type.
+  auto val = lhs.value() * RR::den / U1::den * U1::num +
+             rhs.value() * RR::den / U2::den * U2::num;
+
+  return Duration<decltype(val), RR>{val};
+}
 
 #endif // !CPP_TEMPLATES_CHAPTER23_METAPROGRAMMING
